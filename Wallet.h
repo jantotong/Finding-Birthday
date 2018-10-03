3bd79f6562f84cc326a84f5d599d39aefd1e4d4d
jantotong <43649573+jantotong@users.noreply.github.com>
2018-10-02 18:20:39 -0700

    Update Wallet.h

--- Wallet.h
+++ Wallet.h
@@ -6,18 +6,17 @@ using namespace std;
 class Wallet {
 private:
 	Currency Dollar, Euro, Yen, Rupee, Yuan;
-	Currency wallet[5] = { Dollar, Euro, Yen, Rupee, Yuan };
+	Currency wallet[5] = {Dollar, Euro, Yen, Rupee, Yuan};
 	int numTypes;
 public:
 	Wallet();
 	~Wallet() {}; // destructor
 
-	void typeExists(int x) ;
+	void typeExists(int x);
 	void add(int x, Currency type);
 	void subtract(int x, Currency type);
 	void show(int x);
 	void removeAll();
+	void isEmpty();
 };
-
-
 #endif
